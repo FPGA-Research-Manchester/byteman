9a7297fab6507d3e5b7aac2bc4fcf3fab15963a9
GitHub Action <action@github.com>
2022-04-14 13:08:37 +0000

    Automatic build config

---
+++ build/CMakeFiles/byteman.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/byteman.dir/src/byteman.cpp.o"
+  "CMakeFiles/byteman.dir/src/byteman.cpp.o.d"
+  "byteman"
+  "byteman.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/byteman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
