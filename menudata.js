31df49428b288787a0a346c373c8d8c85b06f3a3
kmanev <kmanev@users.noreply.github.com>
2022-04-19 17:03:35 +0000

    deploy: 0708b337d24faec107dd9a12b7bc1676fec98da1

--- menudata.js
+++ menudata.js
@@ -22,4 +22,5 @@ for the JavaScript code in this file
 */
 var menudata={children:[
 {text:"Main Page",url:"index.html"},
-{text:"Related Pages",url:"pages.html"}]}
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"}]}]}
