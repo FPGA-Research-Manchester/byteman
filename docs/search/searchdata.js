bacde73e11a7192ff683c6b0ada29fe3853b6396
GitHub Action <action@github.com>
2022-04-19 13:44:10 +0000

    Docs

--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,18 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "b",
-  1: "b"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Pages"
 };
 
