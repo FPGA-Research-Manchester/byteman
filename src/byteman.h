1c2ea766a6758cc8da373fac5cd296f0b209af6a
GitHub Action <action@github.com>
2022-04-19 12:46:02 +0000

    Autoversion

--- src/byteman.h
+++ src/byteman.h
@@ -2,7 +2,7 @@
 #define BYTEMAN_H
 //Any changes to version numbers should be done only in CMakeLists.txt, not here!
 #define VERSION "0.0"
-#define VERSION_BUILD "13"
+#define VERSION_BUILD "14"
 //placeholder definition to verify linking
 int x;
 
