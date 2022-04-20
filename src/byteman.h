266d69eafe6c87fa16881de3dd0c4d0cb8f82db7
GitHub Action <action@github.com>
2022-04-20 13:55:25 +0000

    Build #35

--- src/byteman.h
+++ src/byteman.h
@@ -2,7 +2,7 @@
 #define BYTEMAN_H
 //Any changes to version numbers should be done only in CMakeLists.txt, not here!
 #define VERSION "0.0"
-#define VERSION_BUILD "34"
+#define VERSION_BUILD "35"
 //placeholder definition to verify linking
 int x; /** Doxygen test information about int x; */
 
