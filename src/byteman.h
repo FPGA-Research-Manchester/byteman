b0fca136fc365649f880665505224e329c237c1d
GitHub Action <action@github.com>
2022-05-25 11:04:16 +0000

    Build #108

--- src/byteman.h
+++ src/byteman.h
@@ -23,7 +23,7 @@ using namespace std;
 
 //Any changes to version numbers should be done only in CMakeLists.txt, not here!
 #define VERSION "0.5"
-#define VERSION_BUILD "107"
+#define VERSION_BUILD "108"
 
 enum Arch{
 	ARCH_NULL = 0,
