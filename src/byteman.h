ed3345c29aee08585f0e48e485529b9c6a6ac9d7
GitHub Action <action@github.com>
2022-05-26 14:07:50 +0000

    Build #124

--- src/byteman.h
+++ src/byteman.h
@@ -41,7 +41,7 @@ using namespace std;
 
 //Any changes to version numbers should be done only in CMakeLists.txt, not here!
 #define VERSION "0.6"
-#define VERSION_BUILD "123"
+#define VERSION_BUILD "124"
 
 class byteman
 {
