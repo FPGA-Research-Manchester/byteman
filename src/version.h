4608b6fb36af4ad2cce0e6b8d918f43a09ee3154
GitHub Action <action@github.com>
2022-07-05 15:10:28 +0000

    Build #177

--- src/version.h
+++ src/version.h
@@ -19,6 +19,6 @@
 
 //Any changes to version numbers should be done only in the global CMakeLists.txt, not here!
 #define VERSION "1.1"
-#define VERSION_BUILD "176"
+#define VERSION_BUILD "177"
 
 #endif //VERSION_H
